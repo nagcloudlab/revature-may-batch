6c5ea73f10c17419f15132dd73358f8a21eb0eb3
Nagabhushanam <nagcloudlab@gmail.com>
2022-05-20 00:32:37 +0530

    class & object intro

--- code/play-with-java/src/com/example/oo/ClassObjectEx.java
+++ code/play-with-java/src/com/example/oo/ClassObjectEx.java
@@ -5,16 +5,42 @@ import java.util.Scanner;
 
 class Employee {
 
-    int id; // obj variable
+    // 1. Variables
+    // a. class variables // one copy of data for all objects
+    final static String COMPANY_NAME = "revature";
+    static String tnrName;
+
+    // b. obj/instance variables   // separate copy of data for every object
+    final int id;
     String name;
     double salary;
     String address;
 
+    // 2. constructor
     Employee(int id, String name, double salary/* argmunet variable */) {
+        this(id, name, salary, null);
+    }
+
+    Employee(int id, String name, double salary, String address) {
         this.id = id;
         this.name = name;
         this.salary = salary;
+        this.address = address;
     }
+
+    // 3. method(s)  ( functions bound with an object )
+
+    // a. class methods
+    static void doTraining() {
+        System.out.println(Employee.tnrName + " giving training to all employees");
+    }
+
+    // b. obj/instance methods
+    void doLearnAndWork() {
+        System.out.println(this.name + " learning & working ");
+        this.salary = salary + 100;
+    }
+
 
     @Override
     public String toString() {
@@ -33,12 +59,19 @@ class Employee {
 public class ClassObjectEx {
     public static void main(String[] args) {
 
-        Employee e1  = new Employee(101, "Tom", 1000.00 /*parameter*/);
-        Employee e2 =new Employee(102,"Jerry",2000.00);
-        e2.address="US";
+        Employee.tnrName = "Ben";
 
-        System.out.println(e1);
-        System.out.println(e2);
+        Employee e1 = new Employee(101, "Tom", 1000.00);
+        Employee e2 = new Employee(102, "Jerry", 2000.00, "USA");
+
+
+        Employee.doTraining();
+
+        e1.doLearnAndWork();
+        System.out.println(e1.salary);
+
+        e2.doLearnAndWork();
+        System.out.println(e2.salary);
 
 
     }
