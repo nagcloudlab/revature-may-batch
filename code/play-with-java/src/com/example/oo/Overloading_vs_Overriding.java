92d84ab4f859ef6f623a3de28a2d2acd18c8f87e
Nagabhushanam <nagcloudlab@gmail.com>
2022-05-25 01:28:12 +0530

    overload vs overrrinding

---
+++ code/play-with-java/src/com/example/oo/Overloading_vs_Overriding.java
@@ -0,0 +1,44 @@
+package com.example.oo;
+
+class Speaker {
+    int id;
+    String name;
+    // constructor - overloading
+    Speaker(int id) {
+        this.id = id;
+    }
+    Speaker(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    // method
+    void doSpeak(String topic) {
+        System.out.println(this.name + " quick intro " + topic);
+    }
+    void doSpeak(String topic, int duration) {
+        System.out.println(this.name + " brief intro " + topic);
+    }
+}
+
+class Training{
+    void doTraining(){
+        System.out.println("doTraining()");
+    }
+}
+
+class ClassRoomTraining extends Training{
+    void doTraining(){
+        System.out.println("doTraining()");
+    }
+}
+
+public class Overloading_vs_Overriding {
+
+    public static void main(String[] args) {
+
+        Speaker speaker = new Speaker(12, "Nag");
+        speaker.doSpeak("topic", 10);
+
+    }
+
+}
