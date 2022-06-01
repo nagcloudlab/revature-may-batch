ec760519337f4c1686a717403bc2aca04b3d175e
Nagabhushanam <nagcloudlab@gmail.com>
2022-06-01 21:40:13 +0530

    Ex4

---
+++ code/play-with-java/src/com/example/exercise/Ex4.java
@@ -0,0 +1,22 @@
+package com.example.exercise;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex4 {
+
+    public static void main(String[] args) {
+
+
+        List<String> menu=new ArrayList<>();
+        menu.add("veg");
+        menu.add("nveg");
+        menu.add("veg");
+        menu.add("nveg");
+        menu.add("veg");
+
+        // remove all 'nveg' items from above lis
+
+    }
+
+}
