da40c6f4f32ca19b223325dee2dd6fca1439ec79
Nagabhushanam <nagcloudlab@gmail.com>
2022-06-01 22:02:56 +0530

    Ex5

--- code/play-with-java/src/com/example/exercise/Ex4.java
+++ code/play-with-java/src/com/example/exercise/Ex4.java
@@ -1,6 +1,7 @@
 package com.example.exercise;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Ex4 {
@@ -8,14 +9,27 @@ public class Ex4 {
     public static void main(String[] args) {
 
 
-        List<String> menu=new ArrayList<>();
-        menu.add("veg");
+        List<String> menu = new ArrayList<>();
+        menu.add("nveg");
         menu.add("nveg");
         menu.add("veg");
         menu.add("nveg");
         menu.add("veg");
 
-        // remove all 'nveg' items from above lis
+
+        // way-1
+//        Iterator<String> iterator = menu.iterator();
+//        while (iterator.hasNext()) {
+//            String item = iterator.next();
+//            if (item.equals("nveg"))
+//                iterator.remove();
+//        }
+
+        //way-2
+        menu.removeIf(item -> item.equals("nveg"));
+
+        System.out.println(menu);
+
 
     }
 
---
+++ code/play-with-java/src/com/example/exercise/Ex5.java
@@ -0,0 +1,20 @@
+package com.example.exercise;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex5 {
+
+    public static void main(String[] args) {
+        List<String> menu = new ArrayList<>();
+        menu.add("nveg");
+        menu.add("nveg");
+        menu.add("veg");
+        menu.add("nveg");
+        menu.add("veg");
+
+        // replacce all 'nevg; item with '*'
+
+    }
+
+}
