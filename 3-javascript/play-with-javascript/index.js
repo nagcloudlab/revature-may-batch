fb463c4e0e2853eec5c38d52553612b6e0e165ef
Nagabhushanam <nagabhushanam.n@revature.com>
2022-06-16 02:11:49 +0530

    web ui

--- 3-javascript/play-with-javascript/index.js
+++ 3-javascript/play-with-javascript/index.js
@@ -1,2 +1,14 @@
+//-----------------------------
+// Using DOM Api
+//-----------------------------
 
+// step-1 : select/query required DOM elemnts
 
+const alertBox = document.querySelector(".alert");
+const greetBtn = document.querySelector(".btn");
+
+// step-2 : bind event Listeners , on even react
+
+greetBtn.addEventListener("click", (e) => {
+  alertBox.innerText = "Welcome " + alertBox.innerText;
+});
