989aaebccaae1b914ba0f686a1b9907e36d4c606
Nagabhushanam <nagabhushanam.n@revature.com>
2022-06-16 20:11:10 +0530

    ex

--- 3-javascript/play-with-javascript/index.js
+++ 3-javascript/play-with-javascript/index.js
@@ -1,14 +1,46 @@
-//-----------------------------
-// Using DOM Api
-//-----------------------------
+//---------------------------------------
+// using DOM api
+//---------------------------------------
 
-// step-1 : select/query required DOM elemnts
+const box = document.querySelector(".alert");
+const showBtn = document.querySelector(".btn-success");
+const hideBtn = document.querySelector(".btn-danger");
+const timeBtn = document.querySelector(".btn-dark");
 
-const alertBox = document.querySelector(".alert");
-const greetBtn = document.querySelector(".btn");
-
-// step-2 : bind event Listeners , on even react
-
-greetBtn.addEventListener("click", (e) => {
-  alertBox.innerText = "Welcome " + alertBox.innerText;
+hideBtn.addEventListener("click", (e) => {
+  box.style.display = "none";
 });
+showBtn.addEventListener("click", (e) => {
+  box.style.display = "block";
+});
+timeBtn.addEventListener("click", (e) => {
+  box.innerText = new Date().toLocaleTimeString("en-US", {
+    timeZone: "America/New_york",
+  });
+});
+
+//---------------------------------------
+// using DOM api + Timer Api
+//---------------------------------------
+
+const timeSpan = document.getElementById("time-span");
+
+setInterval(() => {
+  timeSpan.innerText = new Date().toLocaleTimeString("en-US", {
+    timeZone: "America/New_york",
+  });
+}, 1000);
+
+const images = ["images/1.jpeg", "images/2.jpeg", "images/3.jpeg"];
+
+const imgEle = document.getElementById("pov-img");
+
+let i = 0;
+setInterval(() => {
+  const imagePath = images[i];
+  imgEle.src = imagePath;
+  i++;
+  if (i === images.length) {
+    i = 0;
+  }
+}, 1000);
