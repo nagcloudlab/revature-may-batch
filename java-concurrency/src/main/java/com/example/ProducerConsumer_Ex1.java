dc78a73c3ccf53845d196e3b47fb8312fb188d2e
Nagabhushanam <nagcloudlab@gmail.com>
2022-06-24 03:16:48 +0530

    QB

---
+++ java-concurrency/src/main/java/com/example/ProducerConsumer_Ex1.java
@@ -0,0 +1,84 @@
+package com.example;
+
+public class ProducerConsumer_Ex1 {
+
+    private static Object lock = new Object();
+
+    private static int[] buffer;
+    private static int count;
+
+    static class Producer {
+
+        void produce() {
+            synchronized (lock) {
+                if (isFull(buffer)) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                buffer[count++] = 1;
+                lock.notify();
+            }
+        }
+    }
+
+    static class Consumer {
+
+        void consume() {
+            synchronized (lock) {
+                if (isEmpty(buffer)) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                buffer[--count] = 0;
+                lock.notify();
+            }
+        }
+    }
+
+    static boolean isEmpty(int[] buffer) {
+        return count == 0;
+    }
+
+    static boolean isFull(int[] buffer) {
+        return count == buffer.length;
+    }
+
+    public static void main(String... strings) throws InterruptedException {
+
+        buffer = new int[10];
+        count = 0;
+
+        Producer producer = new Producer();
+        Consumer consumer = new Consumer();
+
+        Runnable produceTask = () -> {
+            for (int i = 0; i < 50; i++) {
+                producer.produce();
+            }
+            System.out.println("Done producing");
+        };
+        Runnable consumeTask = () -> {
+            for (int i = 0; i < 45; i++) {
+                consumer.consume();
+            }
+            System.out.println("Done consuming");
+        };
+
+        Thread consumerThread = new Thread(consumeTask);
+        Thread producerThread = new Thread(produceTask);
+
+        consumerThread.start();
+        producerThread.start();
+
+        consumerThread.join();
+        producerThread.join();
+
+        System.out.println("Data in the buffer: " + count);
+    }
+}
---
+++ java-concurrency/src/main/java/com/example/WaitNotify_Ex.java
@@ -0,0 +1,2 @@
+package com.example;public class WaitNotify_Ex {
+}
