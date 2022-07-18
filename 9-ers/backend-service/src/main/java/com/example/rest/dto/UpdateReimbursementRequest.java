77fe2690b6cd4ed1abf62cc5d5b149954b0da40d
Nagabhushanam <nagcloudlab@gmail.com>
2022-07-18 22:32:21 +0530

    ERS - with bugs

---
+++ 9-ers/backend-service/src/main/java/com/example/rest/dto/UpdateReimbursementRequest.java
@@ -0,0 +1,2 @@
+package com.example.rest.dto;public class UpdateReimbursementRequest {
+}
---
+++ 9-ers/frontend-service/src/app/auth.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+
+  constructor(
+    private auth:AuthService,
+    private router:Router
+  ){}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    let b=this.auth.isLoggedIn();
+    if(!b){
+      this.router.navigate(['login'])
+      return false;
+    }
+    return true
+  }
+
+}
